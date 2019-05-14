85a6288e48c6e63dc432f60767a2a09a9d255d5e
Slilianyou <li.lian.you163@163.com>
2019-05-14 09:33:54 +0800

    creat project
    creat project

---
+++ LLYEventCalendar/LLYEventCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLYEventCalendar
+//
+//  Created by SK_15 on 2019/5/14.
+//  Copyright © 2019 SK.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
